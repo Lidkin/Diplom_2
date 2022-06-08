26a6a01490c43c083b8c7b76294810fad0b29f97
Lidkin <lidkin79@gmail.com>
2022-06-08 13:59:41 +0300

    ResponseMessage.java and Credentials.java added

---
+++ src/main/java/practicum/ResponseMessage.java
@@ -0,0 +1,44 @@
+package practicum;
+
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseMessage {
+
+    String email;
+    String name;
+
+    public ResponseMessage(){}
+
+    public ResponseMessage(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String errorMessage(String message){
+        Map<String, Object> messageBody = new HashMap<>();
+        messageBody.put("success", false);
+        messageBody.put("message", message);
+
+        Gson gson = new Gson();
+        return gson.toJson(messageBody);
+    }
+
+    public String validResponse(String accessToken, String refreshToken) {
+
+        Map<String, String> customer = new HashMap<>();
+        customer.put("email", email.toLowerCase());
+        customer.put("name", name);
+
+        Map<String, Object> validResponse = new HashMap<>();
+        validResponse.put("success", true);
+        validResponse.put("accessToken", "Bearer " + accessToken);
+        validResponse.put("refreshToken", refreshToken);
+        validResponse.put("user", customer);
+
+        Gson gson = new Gson();
+        return gson.toJson(validResponse);
+    }
+
+}
