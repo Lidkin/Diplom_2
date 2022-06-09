cce0212666e31346a56828261f1c2e00fc05f6d0
Lidkin <lidkin79@gmail.com>
2022-06-10 01:13:33 +0300

    changed CreateOrderErrorTest.java

--- src/test/java/practicum/CreateOrderErrorTest.java
+++ src/test/java/practicum/CreateOrderErrorTest.java
@@ -1,47 +1,61 @@
 package practicum;
 
+import com.google.gson.Gson;
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
+import io.qameta.allure.internal.shadowed.jackson.core.JsonProcessingException;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import practicum.customer.Credentials;
 import practicum.customer.Customer;
 import practicum.customer.CustomerBody;
-import practicum.order.IngredientsNameAndId;
+import practicum.order.Ingredients;
 import practicum.order.Order;
-
-import java.util.Random;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+
 
 public class CreateOrderErrorTest {
 
-    static String email = "lidkin@mail.ru";
-    static String password = "333-kokoko-333";
-    static String name = "Kokofonik";
-
+    static Credentials credentials = new Credentials();
+    static Ingredients ingredients;
+    static {
+        try {
+            ingredients = new Ingredients();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+    static Gson gson = new Gson();
+    static ResponseMessage responseMessage = new ResponseMessage();
     static Customer customer = new Customer();
-    static CustomerBody customerBody = new CustomerBody(email, password, name);
-    static TokenAndResponseBody tokenOrBody = new TokenAndResponseBody();
+    static CustomerBody customerBody = new CustomerBody(
+            credentials.getEmail(),
+            credentials.getPassword(),
+            credentials.getName());
     static String accessToken;
-    IngredientsNameAndId id = new IngredientsNameAndId();
     Order order = new Order();
+    Map<String, List<String>> burger = new HashMap<>(ingredients.buildRandomBurger(1, 1));
 
     @Step("replacing all '0' with the letter 'o' in a random Id of an ingredient")
-    public String getWrongId() {
-        Random random = new Random();
-        int i = random.nextInt(id.getIngredientsId().length);
-        return id.getIngredientsId()[i].replaceAll("0", "o");
+    public Map<String, List<String>> getWrongId() {
+        Map<String, List<String>> wrongBody = new HashMap<>();
+        List<String> wrongId = new ArrayList<>();
+        for(String el: burger.get("id"))
+           wrongId.add(el.replaceAll("0", "o"));
+        wrongId.addAll(burger.get("id"));
+        wrongBody.put("ingredients", wrongId);
+        return wrongBody;
     }
 
     @BeforeClass
     public static void getToken() throws InterruptedException {
         Thread.sleep(1000);
-        Response before = customer.doRegister(customerBody);
-        accessToken = tokenOrBody.token(before).get(0);
+        accessToken = customer.doRegister(customerBody).body().path("accessToken").toString().substring(7);
     }
 
     @AfterClass
@@ -54,12 +68,12 @@ public class CreateOrderErrorTest {
     @DisplayName("Test with an empty list of ingredients")
     @Test
     public void emptyIngredientsTest() throws InterruptedException {
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         String body = "{\"ingredients\": []}";
         Response response = order.doCreateOrder(body);
         response.then().assertThat().statusCode(400);
-        String actualResponseBody = response.body().prettyPrint();
-        String expectedResponseBody = tokenOrBody.emptyIngredientsOrderMessageBody();
+        String actualResponseBody = response.body().asString();
+        String expectedResponseBody = responseMessage.errorMessage("Ingredient ids must be provided");
         assertEquals(expectedResponseBody, actualResponseBody);
     }
 
@@ -68,12 +82,12 @@ public class CreateOrderErrorTest {
     @DisplayName("Test with an empty list of ingredients for authorized customer")
     @Test
     public void emptyIngredientsAuthorizedCustomerTest() throws InterruptedException {
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         String body = "{\"ingredients\": []}";
         Response response = order.doCreateOrderWithToken(body,accessToken);
         response.then().assertThat().statusCode(400);
-        String actualResponseBody = response.body().prettyPrint();
-        String expectedResponseBody = tokenOrBody.emptyIngredientsOrderMessageBody();
+        String actualResponseBody = response.body().asString();
+        String expectedResponseBody = responseMessage.errorMessage("Ingredient ids must be provided");
         assertEquals(expectedResponseBody, actualResponseBody);
     }
 
@@ -82,12 +96,8 @@ public class CreateOrderErrorTest {
     @DisplayName("Create order with invalid ingredient id")
     @Test
     public void invalidIngredientIdTest() throws InterruptedException {
-        Thread.sleep(2000);
-        String wrongId = getWrongId();
-        for (String idIngredient : id.getIngredientsId())
-            assertNotEquals(idIngredient, wrongId);
-        String body = "{\"ingredients\": [\"" + wrongId + "\"]}";
-        Response response = order.doCreateOrder(body);
+        Thread.sleep(1000);
+        Response response = order.doCreateOrder(gson.toJson(getWrongId()));
         response.then().assertThat().statusCode(500);
         response.body().prettyPrint();
     }
@@ -97,12 +107,8 @@ public class CreateOrderErrorTest {
     @DisplayName("Create order with invalid ingredient id for authorized customer.")
     @Test
     public void invalidIngredientIdAuthorizedCustomerTest() throws InterruptedException {
-        Thread.sleep(2000);
-        String wrongId = getWrongId();
-        for (String idIngredient : id.getIngredientsId())
-            assertNotEquals(idIngredient, wrongId);
-        String body = "{\"ingredients\": [\"" + wrongId + "\"]}";
-        Response response = order.doCreateOrderWithToken(body, accessToken);
+        Thread.sleep(1000);
+        Response response = order.doCreateOrderWithToken(gson.toJson(getWrongId()), accessToken);
         response.then().assertThat().statusCode(500);
         response.body().prettyPrint();
     }
