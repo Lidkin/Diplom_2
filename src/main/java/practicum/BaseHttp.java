0209c9f2bc256245a582e8406de5ba23e0bd03de
Lidkin <lidkin79@gmail.com>
2022-06-15 20:25:53 +0300

    added baseURI and logging to spec

--- src/main/java/practicum/BaseHttp.java
+++ src/main/java/practicum/BaseHttp.java
@@ -1,58 +1,74 @@
 package practicum;
 
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.config.RedirectConfig;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.config.SSLConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-
 import static io.restassured.RestAssured.given;
+
 
 public class BaseHttp {
 
-    private final String baseURI = "https://stellarburgers.nomoreparties.site";
     private final RestAssuredConfig config = RestAssuredConfig.newConfig()
             .sslConfig(new SSLConfig().relaxedHTTPSValidation())
             .redirect(new RedirectConfig().followRedirects(true));
-    private final RequestSpecification specification = given().config(config).header("Content-Type", "application/json");
+
+    private final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("https://stellarburgers.nomoreparties.site")
+            .setContentType(ContentType.JSON)
+            .setConfig(config)
+            .log(LogDetail.METHOD)
+            .log(LogDetail.BODY)
+            .addFilter(new ResponseLoggingFilter(LogDetail.STATUS))
+            .build();
 
     protected Response doGetWithTokenRequest(String api, String token) {
-        return given().config(config)
+        return given().spec(requestSpec)
                 .auth().oauth2(token)
-                .get(baseURI + api);
+                .get(api);
     }
 
     protected Response doGetRequest(String api) {
-        return specification.get(baseURI + api);
+        return given().spec(requestSpec)
+                .get(api);
     }
 
-    protected Response doPostRequest(String api, Object body){
-        return specification
+    protected Response doPostRequest(String api, Object body) {
+        return given().spec(requestSpec)
                 .body(body)
-                .post(baseURI + api);
+                .post(api);
     }
 
-    protected Response doPostRequestWithParam(String api, Object body, String token){
-        return specification
+    protected Response doPostRequestWithParam(String api, Object body, String token) {
+        return given().spec(requestSpec)
                 .auth().oauth2(token)
                 .body(body)
-                .post(baseURI + api);
+                .post(api);
     }
-    protected Response doPatchRequestWithParam(String api, Object body, String token){
-        return specification
+
+    protected Response doPatchRequestWithParam(String api, Object body, String token) {
+        return given()
+                .spec(requestSpec)
                 .auth().oauth2(token)
                 .body(body)
-                .patch(baseURI + api);
+                .patch(api);
     }
 
-    protected Response doPatchRequest(String api){
-        return specification.patch(baseURI + api);
+    protected Response doPatchRequest(String api, Object body) {
+        return given().spec(requestSpec)
+                .body(body)
+                .patch(api);
     }
 
-    protected Response doDeleteRequest(String api, String token){
-        return given().config(config)
+    protected Response doDeleteRequest(String api, String token) {
+        return given().spec(requestSpec)
                 .auth().oauth2(token)
-                .delete(baseURI + api);
+                .delete(api);
     }
 
 }
